0054eda3074da3202ec1be1250fefce86abe151d
Szabolcs <fabianszabi17@gmail.com>
2021-05-31 18:10:56 +0300

    Logger attempt

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -12,63 +12,89 @@ public class Main {
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8999).usePlaintext().build();
 
-        ChatServiceGrpc.ChatServiceStub ChatStub=ChatServiceGrpc.newStub(channel);
-        System.out.println("1) Add a person");
-        System.out.println("0) Exit");
-        System.out.println("Introduceti o optiune: ");
-        int option = -1;
-        while (option != 0)
-        {
+        ChatServiceGrpc.ChatServiceStub ChatStub = ChatServiceGrpc.newStub(channel);
+        System.out.println("Adding your name!");
+        System.out.print("Introduceti Numele: ");
+        Scanner read = new Scanner(System.in);
+        String nume = read.nextLine();
+        while (nume.length() == 0) {
+            System.out.println("introduceti o nume normala");
+            nume = read.nextLine();
+        }
 
-            Scanner sc=new Scanner(System.in);
-            option=sc.nextInt();
+            ChatStub.replyServer(
+                    ChatApp.FromClient.newBuilder().setName(nume).setMessage("default").build(),
+                    new StreamObserver<ChatApp.LoginMessage>() {
+                        @Override
+                        public void onNext(ChatApp.LoginMessage reply) {
+                            System.out.println(reply);
+                            System.out.println("1) Send a message");
+                            System.out.println("0) Exit");
+                            System.out.println("Introduceti o optiune: ");
+                        }
 
+                        @Override
+                        public void onError(Throwable throwable) {
+                            System.out.println("Error: " + throwable.getMessage());
+                        }
 
-            switch(option)
-            {
-                case 1:
-                    Scanner read = new Scanner(System.in);
-                    System.out.print("Introduceti Numele: ");
-                    String nume = read.nextLine();
-                    System.out.print("Introduceti Message: ");
-                    String message = read.nextLine();
-        ChatStub.replyServer(
-                ChatApp.FromClient.newBuilder().setName(nume).setMessage(message).build(),
-                new StreamObserver<ChatApp.LoginMessage>()
-                {
-                    @Override
-                    public void onNext(ChatApp.LoginMessage reply)
-                    {
-                        System.out.println(reply);
-                        System.out.println("1) Add a person");
-                        System.out.println("0) Exit");
-                        System.out.println("Introduceti o optiune: ");
+                        @Override
+                        public void onCompleted() {
+
+                        }
                     }
+            );
 
-                    @Override
-                    public void onError(Throwable throwable)
-                    {
-                        System.out.println("Error: " + throwable.getMessage());
-                    }
 
-                    @Override
-                    public void onCompleted()
-                    {
+            System.out.println(("1) Sending other message"));
+            System.out.println("0) Exit");
+            System.out.println("Introduceti o optiune: ");
+            int option = -1;
+            while (option != 0) {
 
-                    }
+                Scanner sc = new Scanner(System.in);
+                option = sc.nextInt();
+
+
+                switch (option) {
+                    case 1:
+
+                        System.out.print("Introduceti Message: ");
+                        String message = read.nextLine();
+
+                        ChatStub.messageResponse(
+                                ChatApp.FromClient.newBuilder().setName(nume).setMessage(message).build(),
+                                new StreamObserver<ChatApp.MessageSent>() {
+                                    @Override
+                                    public void onNext(ChatApp.MessageSent reply) {
+                                        System.out.println(reply);
+                                        System.out.println("1)Send a message");
+                                        System.out.println("0) Exit");
+                                        System.out.println("Introduceti o optiune: ");
+                                    }
+
+                                    @Override
+                                    public void onError(Throwable throwable) {
+                                        System.out.println("Error: " + throwable.getMessage());
+                                    }
+
+                                    @Override
+                                    public void onCompleted() {
+
+                                    }
+                                }
+                        );
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        System.out.println("Nu stiu comanda asta!");
+                        break;
                 }
-        );
-                break;
-                case 0:
-                    break;
-                default:
-                    System.out.println("Nu stiu comanda asta!");
-                    break;
+
             }
+            channel.shutdown();
 
         }
-        channel.shutdown();
-
     }
-}
 

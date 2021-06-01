c941ba0092d7d31ec2998873ebbbcc1b2282ed95
andreea_arvatu <arvatuandreea@yahoo.com>
2021-06-01 18:49:07 +0300

    Created newController.

---
+++ Client/src/main/java/controllers/newController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class newController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
